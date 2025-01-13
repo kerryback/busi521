9f39b6145f2b9741d7df773f8f01ec25cd10459e
Kerry Back <kerryback@gmail.com>
2025-01-13 09:02:54 -0600

    restored pdfs

--- push.py
+++ push.py
@@ -1,8 +1,5 @@
 import os
 os.system("git pull origin main")
-os.system("quarto render")
-with open("docs/CNAME", "w") as f:
-    f.write("https://busi521.kerryback.com")
 os.system("git add .")
 os.system("git commit -m 'update'")
 os.system("git push origin main")

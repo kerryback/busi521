f367e091ab50e29be7162016f5b6d78c6e8b3c42
Kerry Back <kerryback@gmail.com>
2025-01-13 08:45:38 -0600

    'update'

--- push.py
+++ push.py
@@ -1,5 +1,8 @@
 import os
+os.system("git pull origin main")
+os.system("quarto render")
+with open("docs/CNAME", "w") as f:
+    f.write("https://busi521.kerryback.com")
 os.system("git add .")
-string = "my message"
-os.system("git commit -m %s" % string)
+os.system("git commit -m 'update'")
 os.system("git push origin main")
